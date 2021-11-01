ea6eb5a525ef966b4b2e1a33162511653ab5b706
Giordano Rossa <gdmrossa@inf.ufpel.edu.br>
2021-11-01 15:45:58 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css'; /*importa o estilo customizado*/
 import App from './App';
 
 ReactDOM.render(
