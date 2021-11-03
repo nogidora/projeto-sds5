841a6bb96193d253c9b0bb24684105d6c05fe1f4
Giordano Rossa <gdmrossa@inf.ufpel.edu.br>
2021-11-03 09:07:19 -0300

    Mock up Update 2

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -13,32 +13,40 @@ const  DataTable = () =>{
                 </thead>
                 <tbody>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/10/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>29/09/2021</td>
                         <td>Anakin</td>
                         <td>25</td>
                         <td>24</td>
-                        <td>15017.00</td>
+                        <td>449138.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>01/11/2021</td>
                         <td>Kal-El</td>
                         <td>75</td>
                         <td>19</td>
-                        <td>15017.00</td>
+                        <td>466867.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>26/06/2021</td>
                         <td>Logan</td>
                         <td>50</td>
                         <td>50</td>
-                        <td>15017.00</td>
+                        <td>292426.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>30/05/2021</td>
                         <td>Padmé</td>
                         <td>31</td>
                         <td>26</td>
-                        <td>15017.00</td>
+                        <td>450088.00</td>
                     </tr>
                                         
                 </tbody>
